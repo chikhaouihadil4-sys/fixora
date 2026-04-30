c7745cca9b8bf1da7996f58a5d4bf23e2b7114f0
hadil <chikhaouihadil4@gmail.com>
2026-04-30 20:45:14 +0100

    force create admin

--- fixora/settings.py
+++ fixora/settings.py
@@ -168,10 +168,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 from django.contrib.auth.models import User
 
-if os.environ.get('RENDER'):
+try:
     if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser(
             username='admin',
             email='admin@gmail.com',
             password='admin123'
         )
+except:
+    pass
